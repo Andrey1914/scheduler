5de7b92105a980991cd8074981bcfc9aa4fef7ec
Andrey1914 <anlikajw@gmail.com>
2022-09-23 20:07:32 +0200

    added components

---
+++ src/components/Task/Task.jsx
@@ -0,0 +1,19 @@
+import { useDispatch } from 'react-redux';
+import { MdClose } from 'react-icons/md';
+import { deleteTask, toggleCompleted } from 'redux/tasksSlice';
+
+export const Task = ({ task }) => {
+  const dispatch = useDispatch();
+  const handleDelete = () => dispatch(deleteTask(task.id));
+  const handleToggle = () => dispatch(toggleCompleted(task.id));
+
+  return (
+    <div>
+      <input type="checkbox" checked={task.completed} onChange={handleToggle} />
+      <p>{task.text}</p>
+      <button onClick={handleDelete}>
+        <MdClose size={24} />
+      </button>
+    </div>
+  );
+};
